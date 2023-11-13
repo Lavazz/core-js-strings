48b63516f45de151925657e9aa4f8562930141b0
Katsiaryna_Kaloshych <katsiaryna_kaloshych@epam.com>
2023-11-13 13:15:44 +0300

    feat: add more decisions

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -254,8 +254,8 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  return seconds.toString().padStart(5, `0${minutes}:`);
 }
 
 /**
